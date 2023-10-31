ee7176e497e6086c9c5fe74e7381a3b4471ffeb4
Abhishek Singh <80335380+16abhip@users.noreply.github.com>
2023-10-31 14:03:49 +0530

    Update moduless.js

--- moduless.js
+++ moduless.js
@@ -8,13 +8,11 @@ const {
 } = require("./mongocon");
 // mongodb+srv://abhip:trwDq9IaYq30IJOk@cluster0.gybwhw7.mongodb.net/
 //trwDq9IaYq30IJOk
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const port = 8000;
 const ObjectId = mongoose.Types.ObjectId;
 app.use(express.json());
-app.use('/api', createProxyMiddleware({
-  target: 'https://tiny-blue-oyster.cyclic.app',
-  changeOrigin: true,
-}));
+
 module.exports = { express, User, Product, mongoose, app, port, ObjectId, userAdder, productAdder };
