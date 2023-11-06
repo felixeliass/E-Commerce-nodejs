6b389c5769684e45ddbeadf6a11befddaba30d44
Abhishek Singh <80335380+16abhip@users.noreply.github.com>
2023-11-06 23:25:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -335,10 +335,10 @@ app.get("/api/users/login", async (req, res) => {
     if (data === null) {
       return res.status(401).json({});
     }
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (err) {
     console.log(err);
-    res.status(200).json({});
+    return res.status(200).json({});
   }
 });
 
